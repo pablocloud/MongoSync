0b6ec805a082669aa4c034e257cf3977ce09d41d
Pablo Verdugo Huerta <pablo.verdugo000@gmail.com>
2016-10-11 14:17:16 +0200

    .

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -1,14 +1,15 @@
 package model;
 
 import classes.*;
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
 import factories.ThreadsFactory;
+import org.bson.Document;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -64,28 +65,52 @@ public class Task extends Thread {
     @Override
     public void run() {
         ExecutorService executorService = Executors.newSingleThreadExecutor(ThreadsFactory.getInstance());
+        MongoClient mongoClientFinal = new Connection().getConnection(clientTo);
+        MongoDatabase database = mongoClientFinal.getDatabase(collection.getDatabaseFinal());
+        MongoIterable<String> strings = database.listCollectionNames();
+        boolean exist = false;
+        for(String col : strings){
+            if(col.equals(collection.getNameFinal())){
+                exist = true;
+            }
+        }
+        if(!exist){
+            database.createCollection(collection.getNameFinal());
+        }
+        MongoCollection<Document> collectionResult = database.getCollection(this.collection.getNameFinal());
+        if(collectionResult.count() <= 0){
+            MongoClient mongoClientOrigin = new Connection().getConnection(clientFrom);
+            MongoDatabase databaseOrigin = mongoClientOrigin.getDatabase(collection.getDatabaseOrigin());
+            MongoCollection<Document> collectionOrigin = databaseOrigin.getCollection(this.collection.getNameOrigin());
+            Document id = collectionOrigin.find().sort(new BasicDBObject("_id", 1)).limit(1).first();
+            collectionResult.insertOne(id);
+            mongoClientOrigin.close();
+        }
+        mongoClientFinal.close();
         try {
-            Query query = new Query(clientFrom, clientTo, collection);
+            Query query = new Query(clientFrom, clientTo, this.collection);
             query.run();
             while (query.isAlive()) {
-                Thread.sleep(100);
+                Thread.sleep(1);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if (collection.getDiff() < maxDiff) {
-            ArrayList<Thread> threadList = new ArrayList<>();
-            threadList.add(new Dump(clientFrom, clientTo, collection, parameters));
-            threadList.add(new Restore(clientTo, collection, parameters));
-            threadList.forEach(Thread::run);
-        } else {
-            ArrayList<Thread> threadList = new ArrayList<>();
-            threadList.add(new PylonHammer(getClientFrom(), getClientTo(), getCollection(), maxDiff));
-            threadList.forEach(Thread::run);
-        }
-        if(collection.getIndexes() != null){
-            for(IndexField indexField : collection.getIndexes()){
-                new Index(clientTo, collection, indexField).run();
+        if(this.collection.getDiff() > 0){
+            if (this.collection.getDiff() < maxDiff) {
+                ArrayList<Thread> threadList = new ArrayList<>();
+                threadList.add(new Dump(clientFrom, clientTo, this.collection, parameters));
+                threadList.add(new Restore(clientTo, this.collection, parameters));
+                threadList.forEach(Thread::run);
+            } else {
+                ArrayList<Thread> threadList = new ArrayList<>();
+                threadList.add(new PylonHammer(getClientFrom(), getClientTo(), getCollection(), maxDiff));
+                threadList.forEach(Thread::run);
+            }
+            if(this.collection.getIndexes() != null){
+                for(IndexField indexField : this.collection.getIndexes()){
+                    new Index(clientTo, this.collection, indexField).run();
+                }
             }
         }
     }
