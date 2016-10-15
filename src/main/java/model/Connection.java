6afcd009ac0635d4d496411d73cb375de3dd46c4
Eduardo Espinosa <eduardo_espinos@hotmail.com>
2016-10-15 02:00:19 +0200

    Refactor Connection Class
    Check if all fields are valid

--- src/main/java/model/Connection.java
+++ src/main/java/model/Connection.java
@@ -8,6 +8,8 @@ import java.util.Objects;
 
 public class Connection {
 
+    private Client client;
+
     /**
      * Returns a MongoClient with the config data from Client.
      *
@@ -15,14 +17,47 @@ public class Connection {
      * @return MongoClient.
      */
     public MongoClient getConnection(Client client) {
-        String connection;
-        if (!Objects.equals(client.getUsername(),null) && !Objects.equals(client.getPassword(),null)) {
-            connection = "mongodb://" + client.getUsername() + ":" + client.getPassword() + "@" + client.getHost() + ":" + client.getPort();
-        } else {
-            connection = "mongodb://" + client.getHost() + ":" + client.getPort();
-        }
-        MongoClientURI uri = new MongoClientURI(connection);
-        return new MongoClient(uri);
-    }
+        this.client = new Client();
+        String connection = "";
 
+        try {
+            if (client.getHost() != null && !client.getHost().isEmpty()) {
+                this.client.setHost(client.getHost());
+            } else {
+                throw new Exception("Server not set");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        try {
+            if (client.getPort() != null && !client.getPort().isEmpty()) {
+                this.client.setPort(client.getPort());
+            } else {
+                throw new Exception("Port not set");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(2);
+        }
+
+        try {
+            if ((client.getUsername() == null || client.getUsername().isEmpty()) && (client.getPassword() == null || client.getPassword().isEmpty()) && (client.getAuthDB() == null || client.getAuthDB().isEmpty())) {
+                connection = "mongodb://" + this.client.getHost() + ":" + this.client.getPort();
+            } else if ((client.getUsername() != null && !client.getUsername().isEmpty()) && (client.getPassword() != null && !client.getPassword().isEmpty()) && (client.getAuthDB() != null && !client.getAuthDB().isEmpty())) {
+                this.client.setUsername(client.getUsername());
+                this.client.setPassword(client.getPassword());
+                this.client.setAuthDB(client.getAuthDB());
+                connection = "mongodb://" + this.client.getUsername() + ":" + this.client.getPassword() + "@" + this.client.getHost() + ":" + this.client.getPort();
+            } else {
+                throw new Exception("User, password and/or authDB not set");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(3);
+        }
+        return new MongoClient(new MongoClientURI(connection));
+    }
 }
