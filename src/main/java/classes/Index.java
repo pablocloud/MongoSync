2e950f843674037528c54d19954417471607c0a9
Eduardo Espinosa <eduardo_espinos@hotmail.com>
2016-10-18 07:39:50 +0200

    Fix Index Class

--- src/main/java/classes/Index.java
+++ src/main/java/classes/Index.java
@@ -18,19 +18,36 @@ public class Index extends Thread {
     private IndexField index;
     private SyncLogger syncLogger = SyncLogger.getInstance();
 
+    private MongoClient to;
+
     public Index (Client client, Collection collection, IndexField index){
         this.client = client;
         this.collection = collection;
         this.index = index;
     }
 
-    @Override
-    public void run() {
+    public Index (MongoClient to, Collection collection, IndexField index){
+        this.to = to;
+        this.collection = collection;
+        this.index = index;
+    }
+
+    private void oldWay(){
         MongoClient mongoClient = Connection.getInstance().getConnection(client);
         MongoDatabase database = mongoClient.getDatabase(collection.getDatabaseFinal());
         MongoCollection<Document> collectionDb = database.getCollection(this.collection.getNameFinal());
         syncLogger.logMessage("ID THREAD INDEX   : " + String.valueOf(Thread.currentThread().getId()) + " : " + Instant.now().toString() + "     " + collection.getNameFinal(), SyncLogger.ANSI_CYAN, false);
         collectionDb.createIndex(new BasicDBObject(this.index.getName(), this.index.getOrder()), new IndexOptions().background(true));
         mongoClient.close();
+    }
+
+
+
+    @Override
+    public void run() {
+        MongoDatabase database = to.getDatabase(collection.getDatabaseFinal());
+        MongoCollection<Document> collectionDb = database.getCollection(this.collection.getNameFinal());
+        syncLogger.logMessage("ID THREAD INDEX   : " + String.valueOf(Thread.currentThread().getId()) + " : " + Instant.now().toString() + "     " + collection.getNameFinal(), SyncLogger.ANSI_CYAN, false);
+        collectionDb.createIndex(new BasicDBObject(this.index.getName(), this.index.getOrder()), new IndexOptions().background(true));
     }
 }
